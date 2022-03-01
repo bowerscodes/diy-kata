bfdd092a1b3fb84591832dc50f0ec407154020c3
James Bowers <jamesbowers123@gmail.com>
2022-03-01 10:44:10 +0000

    complete numbersToReversedDigits tests

--- src/kata3.numberToReversedDigits.js
+++ src/kata3.numberToReversedDigits.js
@@ -1,3 +1,8 @@
-const numberToReversedDigits = number => {};
+const numberToReversedDigits = number => {
+    const digits = String(number)
+    .split('')
+    .map(Number)
+    return digits.reverse();
+};
 
 module.exports = numberToReversedDigits;
--- test/kata3.numberToReversedDigits.test.js
+++ test/kata3.numberToReversedDigits.test.js
@@ -1,5 +1,10 @@
 const { numberToReversedDigits } = require("../src");
 
 describe("numberToReversedDigits", () => {
-  test("returns a reversed array of the number's digits", () => {});
+  it("returns a reversed array of the number's digits", () => {
+    expect(numberToReversedDigits(12345)).toEqual([5, 4, 3, 2, 1])
+    expect(numberToReversedDigits(36912)).toEqual([2, 1, 9, 6, 3])
+    expect(numberToReversedDigits(5101520)).toEqual([0, 2, 5, 1, 0, 1, 5])
+
+  });
 });
