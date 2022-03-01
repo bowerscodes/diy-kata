65d8082ac9fe9b2f578f6d93f9ccca9dc37d3a30
James Bowers <jamesbowers123@gmail.com>
2022-03-01 16:26:20 +0000

    complete joinNames tests

--- src/kata6.joinNames.js
+++ src/kata6.joinNames.js
@@ -1,3 +1,7 @@
-const joinNames = namesObj => {};
+const joinNames = namesObj => {
+    return namesObj.map(function(elem){
+        return elem.name;
+    }).join(', ').replace(/, ([^,]*)$/, ' & $1')
+};
 
 module.exports = joinNames;
--- test/kata6.joinNames.test.js
+++ test/kata6.joinNames.test.js
@@ -1,5 +1,36 @@
 const { joinNames } = require("../src");
 
 describe("joinNames", () => {
-  test("returns string of names, seperated by commas and an ampersand", () => {});
+  it("returns string of names, seperated by commas and an ampersand", () => {
+    expect(joinNames([{
+
+      name: 'Bart'
+    
+    }, {
+    
+      name: 'Lisa'
+    
+    }, {
+    
+      name: 'Maggie'
+    
+    }])).toBe("Bart, Lisa & Maggie")
+  })
+  expect(joinNames([{
+
+    name: 'Dorothy'
+  
+  }, {
+  
+    name: 'Blanche'
+  
+  }, {
+  
+    name: 'Rose'
+  
+  }, {
+  
+    name: 'Sophia'
+  
+  }])).toBe("Dorothy, Blanche, Rose & Sophia")
 });
